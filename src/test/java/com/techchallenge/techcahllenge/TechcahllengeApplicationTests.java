5cbbd7145d6e4e13be4bfc05b32f611e1d896fa1
flaviohenso <flavio.henso@gmail.com>
2024-05-06 21:17:16 -0300

    Initial commit

---
+++ src/test/java/com/techchallenge/techcahllenge/TechcahllengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.techcahllenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechcahllengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
