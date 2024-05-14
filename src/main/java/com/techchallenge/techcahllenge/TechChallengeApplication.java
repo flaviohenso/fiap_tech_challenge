f8ba10e90b878c87b6fcb4a25875efb18c019ab6
Victor Leite <victor_leite@outlook.com.br>
2024-05-13 21:03:37 -0300

    fix: change application file name

--- src/main/java/com/techchallenge/techcahllenge/TechChallengeApplication.java
+++ src/main/java/com/techchallenge/techcahllenge/TechChallengeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TechcahllengeApplication {
+public class TechChallengeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TechcahllengeApplication.class, args);
+		SpringApplication.run(TechChallengeApplication.class, args);
 	}
 
 }
